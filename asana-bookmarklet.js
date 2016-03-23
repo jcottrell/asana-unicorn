f9b2a6dd21aec659404955eb1293cd018ba59161
Joshua Cottrell <joshua.e.cottrell@gmail.com>
2016-03-23 10:46:23 -0400

    Updated with random choice of celebration

--- asana-bookmarklet.js
+++ asana-bookmarklet.js
@@ -1,21 +1,30 @@
+/* globals AsanaRainbow,env,CommonUrl */
+/* jshint camelcase: false */
+// https://app.asana.com/-/static/apps/asana/media/celebration_unicorn.png
+// https://app.asana.com/-/static/apps/asana/media/celebration_phoenix.png
+// https://app.asana.com/-/static/apps/asana/media/celebration_narwhal.png
+// https://app.asana.com/-/static/apps/asana/media/celebration_yeti.png
 (function (win) {
     'use strict';
-    var r, rain = function () {
-        if (win.AsanaRainbow) {
-            r = AsanaRainbow.create({
-                    'name':'unicorn',
-                    'image':{
-                        'urls':[
-            'https://app.asana.com/-/static/apps/asana/media/unicorn-reskin.png'
-                        ]
-                    }
-                });
-            r.start();
-        }
-    };
+    var r,
+        celebrants,
+        rain = function () {
+            var celebrant;
+            if (win.AsanaRainbow) {
+                loadCelebrants();
+                celebrant = Math.floor(Math.random()*celebrants.length);
+                r = AsanaRainbow.create(celebrants[celebrant]);
+                r.start();
+            }
+        },
+        loadCelebrants = function () {
+            if (AsanaRainbow._RESKIN_CELEBRATIONS) {
+                celebrants = AsanaRainbow._RESKIN_CELEBRATIONS;
+            }
+        };
     if ((win.AsanaRainbow === undefined) && win.env && win.env.dynamic_loader) {
         env.dynamic_loader.load({
-            url:CommonUrl.staticUrl({path:"apps/asana/view/asana_rainbow.js",
+            url:CommonUrl.staticUrl({path:'apps/asana/view/asana_rainbow.js',
                     compute_hash:!1}),
             allow_cached:!1,
             callback: rain
